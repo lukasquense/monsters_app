fdd05b74175db36c28e293352a775dfdade36e1a
lukasquense <lukas.quense@gmail.com>
2020-04-12 17:42:51 -0400

    Updates

---
+++ precache-manifest.91f81fe9521b160f1ce658510eb6f4f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf58b18d7bdc40830072f6794df7e38",
+    "url": "/monsters_app/index.html"
+  },
+  {
+    "revision": "dffdc9928a5cd64df9d9",
+    "url": "/monsters_app/static/css/main.a51cd287.chunk.css"
+  },
+  {
+    "revision": "bd6f1a6767a4b844f3f4",
+    "url": "/monsters_app/static/js/2.ab6545d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters_app/static/js/2.ab6545d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dffdc9928a5cd64df9d9",
+    "url": "/monsters_app/static/js/main.81b764f4.chunk.js"
+  },
+  {
+    "revision": "a2400685674d729b06ab",
+    "url": "/monsters_app/static/js/runtime-main.8b0543aa.js"
+  }
+]);
